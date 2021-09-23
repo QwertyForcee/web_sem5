ea3df65ca15c896e933a51c39045fdae738c4d90
Maxim Bystrov <maximus.bystrov@gmail.com>
2021-09-23 23:30:53 +0300

    lab 1 & 2

---
+++ WEB_953505_Bystrov/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_953505_Bystrov.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<ListDemo> _listDemos = new List<ListDemo>() {
+            new ListDemo{ ListItemText="тридцать три" ,ListItemValue=33},
+            new ListDemo{ ListItemText="двенадцать" ,ListItemValue=12},
+            new ListDemo{ ListItemText="пять" ,ListItemValue=5}
+        };
+        public IActionResult Index()
+        {
+            ViewData["DemoList"] = new SelectList(_listDemos, "ListItemValue", "ListItemText");
+            ViewData["Text"] = "Лабораторная работа 2";
+            return View();
+        }
+    }
+    public class ListDemo
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
+    }
+}
